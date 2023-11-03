7a360ba6613e0c6c53790c685a9af004819ca6ee
Mauricio <mneyraarzapalo@gmail.com>
2023-11-02 19:49:40 -0500

    java escrip tipo de datos

---
+++ semana6/dia01-java-script-basico/js/index.js
@@ -0,0 +1,65 @@
+console.log('Hellow world 2')
+
+// este es un comentario lineal 
+
+/* esto es 
+un comentario 
+de multiples 
+lineas
+*/
+
+
+//tipos de datos
+//Primitivos: Number ,String , boolean(true,false),undenfined
+
+//number
+
+const numero1=20;
+const numero2=20.50;
+const numero3=-36;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+
+console.log(typeof numero1)
+console.log(typeof numero2)
+console.log(typeof numero3)
+
+
+//String o cadena
+
+const alumna='Claudia';
+const alumno="Marco";
+const numero4="200";
+const numero5=200;
+
+console.log(typeof alumna);
+console.log(typeof alumno);
+console.log(typeof numero4);
+console.log(typeof numero5);
+
+
+//boolean --> False o true
+
+const tinedaDescuento=false;
+//const =true;
+
+console.log(tinedaDescuento);
+console.log(typeof tinedaDescuento);
+
+//UNDEFINED
+
+let nombre;
+
+console.log(nombre);
+console.log(typeof nombre);
+
+//null
+
+let apellido=null;
+
+console.log(apellido)
+console.log(typeof apellido);
+
+//
