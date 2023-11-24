8960b7ec5c050dc634f682f6e7c69b721233da7a
Jorge <mneyraarzapalo@gmail.com>
2023-11-23 19:45:53 -0500

    react intro

---
+++ semana8/dia-02-javascrip-es6-fech-countris-modules/modules/constants.js
@@ -0,0 +1,12 @@
+//exprotancion nombrada
+export const regions=[
+    'Africa',
+    'Asia',
+    'Europe'
+]
+
+//exportacion por defecto
+
+const name = 'Mauricio';
+
+export default name;
--- semana8/dia-02-javascrip-es6-fech-countris-modules/modules/mainn.js
+++ semana8/dia-02-javascrip-es6-fech-countris-modules/modules/mainn.js
@@ -138,13 +138,18 @@ document.addEventListener('DOMContentLoaded', async () =>{
 
 // console.log('Hola JS!')
 
-const url = 'https://restcountries.com/v3.1/all?fields=name,flags,region,capital,population,languages,currencies,timezones'
+import{renderCountries, renderResults}from './utils.js'
+import fetchCountries from './servicios.js'
+import Minombre, {regions} from './constants.js'
+
+console.log(Minombre)
+console.log(regions)
 
 let countryData = []
 
 const searchInput = document.querySelector('.app__input')
 const filterSelect = document.querySelector('.app__filter')
-const resultsDiv = document.querySelector('.app__results')
+
 
 // function fetchCountries() {
 //   return fetch(url) // Promise
@@ -195,79 +200,9 @@ filterSelect.addEventListener('input', (event) => {
   renderResults(filteredCountriesByRegion)
 })
 
-// TODO: Reescribir la función fetchCountries usando async/await para luego del receso.
-
-const fetchCountries = async () => {
-  try {
-    const response = await fetch(url) // Promise
-
-    if(!response.ok) {
-      throw new Error('ERROR:', response.status)
-    }
-
-    const json = await response.json()
-
-    return json
-  } catch(error) {
-    console.log(error)
-  }
-}
-
-const renderResults = (countriesFiltered) => {
-  const total = countriesFiltered.length
-
-  resultsDiv.textContent = `${total} Countries Filtered`
-}
-
-const renderCountries = (countries = []) => {
-  console.log(countries)
-
-  const countryListElement = document.querySelector('.app__list')
-
-  let countryList = ''
-
-  if(countries.length === 0) {
-    countryListElement.innerHTML=`
-    <div class="card">
-    <div class='app__list--no--found'>
-    <img src=./images/icon-sad-square.svg width=100px, height=100px >
-    <p>Sorry no result fount</p>
-    </div>
-    </div>
-    `
-    return
-  }
-
-  countryListElement.classList.remove('app__list--no--found')
+ // DOMContentLoaded -> Es un evento del navegador que espera a que el DOM esté cargado
   
-
-  countries.forEach(country => {
-    // console.log(country)
-    countryList += `
-      <div class="country">
-        <img class="country__flag" src="${country.flags.png}" alt="${country.flags.alt}" />
-        <div class="country__wrapper">
-          <h2 class="country__title">${country.name.common}</h2>
-          <div class="country__description">
-            <strong>Population:</strong> ${country.population}
-          </div>
-          <div class="country__description">
-            <strong>Region:</strong> ${country.region}
-          </div>
-          <div class="country__description">
-            <strong>Capital:</strong> ${country.capital[0]}
-          </div>
-        </div>
-      </div>
-    `
-  })
-
-  countryListElement.innerHTML = countryList
-}
-
-// DOMContentLoaded -> Es un evento del navegador que espera a que el DOM esté cargado
-
-document.addEventListener('DOMContentLoaded', async () => {
+ document.addEventListener('DOMContentLoaded', async () => {
   console.log('¡Mi DOM ya cargó!')
   
   // fetchCountries()
